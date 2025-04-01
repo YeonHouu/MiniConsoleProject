a4b03d93fd0590ce19a1ba3ee42e1e305ea8a76d
YeonHouu <yeonhhou@gmail.com>
2025-04-01 15:07:22 +0900

    Feat: Set Game class
    Set 3 base game methods in Game class to run this game.
    1. Start()
    2. Run()
    3. End()

---
+++ MiniTextRPGProject/MiniTextRPG/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTextRPG
+{
+    class Game
+    {
+        public static void Start()
+        {
+
+        }
+
+        public static void Run()
+        {
+
+
+        }
+        public static void End()
+        {
+
+        }
+    }
+}
