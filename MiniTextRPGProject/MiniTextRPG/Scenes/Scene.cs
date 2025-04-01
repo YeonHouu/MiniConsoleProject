c28f38bbf80ed9488cbf51e9d67d54c32269a17a
YeonHouu <yeonhhou@gmail.com>
2025-04-01 15:17:52 +0900

    Feat: Base Scene class
    Feature base abstract Scene class to create various game scenes

---
+++ MiniTextRPGProject/MiniTextRPG/Scenes/Scene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTextRPG.Scenes
+{
+    public abstract class Scene
+    {
+        protected ConsoleKey inputKey;
+
+        //해당 Scene 상황 설명 텍스트
+        public abstract void Render();
+
+        //해당 Scene 선택지
+        public abstract void Choice();
+
+        //해당 Scene 선택지 결과
+        public abstract void Result();
+
+        //해당 Scene 선택지 결과에 따른 다음 Scene
+        public abstract void NextScene();
+
+        //key 입력
+        public void InputKey()
+        {
+            inputKey = Console.ReadKey(true).Key;
+        }
+    }
+}
